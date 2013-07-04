5cb73dbdf8ca1e0ba85fa197a1d8c1b8ec9f96ae
ceshi001 <ceshi001@hhz-4486.sinatay.com>
2013-07-04 16:26:55 +0800

    enhance order flow

--- lwdbiz/src/main/java/org/hsz/lwdbiz/domain/Category.java
+++ lwdbiz/src/main/java/org/hsz/lwdbiz/domain/Category.java
@@ -1,5 +1,28 @@
 package org.hsz.lwdbiz.domain;
 
 public class Category {
+	private Long id;
+	private String name;
 
+	public Category(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
